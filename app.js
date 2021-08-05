2bc60293a161d931b6d3070c2c0b28731fe78fa0
rafeuddaraj <rafeuddaraj2@gmail.com>
2021-08-05 14:50:41 +0600

    All method master Branch added successfully

--- app.js
+++ app.js
@@ -3,10 +3,40 @@ class Person{
         this.name = name
         this.email = email
     }
-    display(){
-        return `I am display???`
+    static create(str){
+        let person = JSON.parse(str)
+        return new Person(person.name,person.email)
+    }
+    address(){
+        return 'Dhaka Bangladesh'
+    }
+    sqr(x){
+        return x*x
     }
 }
-
+let str = '{"name":"Rafe Uddaraj", "email": "rafeuddaraj2@gmail.com"}'
+console.log(Person.create(str));
 let person = new Person()
-console.log(person.display());
+console.log(person.address());
+console.log(new Person().sqr(10));
+
+function any (){
+    this.name = 'rafe'
+}
+
+any.prototype.common = function(){
+    console.log(this);
+}
+
+let an = new any()
+an.common()
+
+function toDo(){
+    this.mail = 'aan76608@gmail.com'
+}
+
+toDo.prototype = Object.create(any.prototype)
+
+let to = new toDo()
+
+to.common()
