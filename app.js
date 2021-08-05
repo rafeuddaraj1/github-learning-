57136209ad1f19210ff8e6cd62547d4c2664af05
rafeuddaraj <rafeuddaraj2@gmail.com>
2021-08-05 14:48:41 +0600

    All method remove and New Display Method create

--- app.js
+++ app.js
@@ -3,40 +3,10 @@ class Person{
         this.name = name
         this.email = email
     }
-    static create(str){
-        let person = JSON.parse(str)
-        return new Person(person.name,person.email)
-    }
-    address(){
-        return 'Dhaka Bangladesh'
-    }
-    sqr(x){
-        return x*x
+    display(){
+        return `I am display???`
     }
 }
-let str = '{"name":"Rafe Uddaraj", "email": "rafeuddaraj2@gmail.com"}'
-console.log(Person.create(str));
+
 let person = new Person()
-console.log(person.address());
-console.log(new Person().sqr(10));
-
-function any (){
-    this.name = 'rafe'
-}
-
-any.prototype.common = function(){
-    console.log(this);
-}
-
-let an = new any()
-an.common()
-
-function toDo(){
-    this.mail = 'aan76608@gmail.com'
-}
-
-toDo.prototype = Object.create(any.prototype)
-
-let to = new toDo()
-
-to.common()
+console.log(person.display());
